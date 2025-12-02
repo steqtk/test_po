ad9cf6d1de1c36bd22f1405e0a3a8eb2bd6753ee
Oleg Nasifulin <setfrom@gmail.com>
2025-12-02 16:45:11 +0300

    Add admin user for backend authentication (login: admin, password: admin123)

---
+++ console/migrations/m251202_100336_add_admin_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Добавляет пользователя admin для доступа в backend
+ * Логин: admin
+ * Пароль: admin123
+ */
+class m251202_100336_add_admin_user extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('{{%user}}', [
+            'username' => 'admin',
+            'auth_key' => Yii::$app->security->generateRandomString(),
+            'password_hash' => Yii::$app->security->generatePasswordHash('admin123'),
+            'email' => 'admin@example.com',
+            'status' => 10, // STATUS_ACTIVE
+            'created_at' => time(),
+            'updated_at' => time(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('{{%user}}', ['username' => 'admin']);
+    }
+}
